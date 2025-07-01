a19dcf7b266058558ffdc3a0a3f59d0eb2fea362
Harsh Gupta <harsh.gupta2211@gmail.com>
2025-07-02 00:00:39 +0530

    add filecoin caliberation support

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -5,6 +5,7 @@ import { PrivyProvider } from "@privy-io/react-auth";
 import PrivySyncProvider from "@/components/PrivySyncProvider";
 import AuthProvider from "@/components/AuthProvider";
 import UserProvider from "@/context/UserContext";
+import { filecoinCalibration } from "viem/chains";
 
 interface Props {
     children: React.ReactNode;
@@ -15,7 +16,11 @@ const ClientProviders: React.FC<Props> = ({ children }) => {
         <AuthProvider>
             <PrivyProvider
                 appId="cmckmrdo700chjl0n51w75eue"
-                config={{ embeddedWallets: { createOnLogin: "users-without-wallets" }, }}
+                config={{
+                    embeddedWallets: { createOnLogin: "users-without-wallets" },
+                    defaultChain: filecoinCalibration,
+                    supportedChains: [filecoinCalibration]
+                }}
             >
                 <UserProvider>
                     <PrivySyncProvider>{children}</PrivySyncProvider>
