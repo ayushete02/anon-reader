e86c987b85be79d09de33390c79cecd73f773e46
Harsh Gupta <harsh.gupta2211@gmail.com>
2025-07-04 23:05:11 +0530

    temp: fix lighthosue

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,12 +1,14 @@
 // NearAI API Configuration
 export const NEAR_AI_CONFIG = {
   API_URL: "https://api.near.ai/v1/chat/completions",
+  IMAGE_API_URL: "https://api.near.ai/v1/images/generations",
   AUTH_HEADER: {
     "Content-Type": "application/json",
     Authorization:
       'Bearer {"account_id":"rishhh.near","public_key":"ed25519:7fgx6fVS7SixNi1PEF6UG3C7fiUDQUPXuh9oCRhkwmCh","signature":"YghVrPYlxcAB+OeYk8iB6amD1wwed5YZIY4Uxsfa7WWCg+n7wMCFwrRO5kd5EqevFtt81126zp0LpuAukfdHCA==","nonce":"1749832648115","recipient":"ai.near","message":"Welcome to NEAR AI","on_behalf_of":null}',
   },
   MODEL: "fireworks::accounts/fireworks/models/mixtral-8x22b-instruct",
+  IMAGE_MODEL: "fireworks::accounts/fireworks/models/playground-v2-1024px-aesthetic",
   PROVIDER: "fireworks",
   MAX_TOKENS: 8192,
   TEMPERATURE: 1,
